4ea9edfb249e273d24159c0fc9a548aed8dcf1e2
amirroohi <amirroohi@yahoo.com>
2024-01-16 19:47:39 +0330

    notes state and it's handlers are added

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,29 @@ import NoteList from "./components/NoteList";
 function App() {
   const [notes, setNotes] = useState([]);
 
-const handleAddNote=(newNote)=>{
-  setNotes((prevNotes) => [...prevNotes, newNote]);
+  const handleAddNote = (newNote) => {
+    setNotes((prevNotes) => [...prevNotes, newNote]);
+  };
 
-}
+  const handleDeleteNote = (id) => {
+    // console.log(id);
+    const filteredNotes = notes.filter((n) => n.id !== id);
+    setNotes(filteredNotes);
+  };
+  const handleCompletedNote = (e) => {
+    const noteId = Number(e.target.value);
+    // 1#when dont need prev state
+    // const newNotes = notes.map((note) =>
+    //   note.id === noteId ? { ...note, completed: !note.completed } : note
+    // );
+    // setNotes(newNotes);
+    // 2#when need prev state (sometimes this way is prefer)
+    setNotes((prevNotes) =>
+      prevNotes.map((note) =>
+        note.id === noteId ? { ...note, completed: !note.completed } : note
+      )
+    );
+  };
 
   return (
     <div className="container">
@@ -17,7 +36,12 @@ const handleAddNote=(newNote)=>{
       <div className="note-app">
         <AddNewNote onAddNote={handleAddNote} />
         <div className="note-container">
-          <NoteList notes={notes} />
+          
+          <NoteList
+            notes={notes}
+            onDelete={handleDeleteNote}
+            onCompleted={handleCompletedNote}
+          />
         </div>
       </div>
     </div>
--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,8 +1,13 @@
-function NoteList({ notes }) {
+function NoteList({ notes, onDelete, onCompleted }) {
   return (
     <div className="note-list">
       {notes.map((note) => (
-        <NoteItem key={note.id} note={note} />
+        <NoteItem
+          key={note.id}
+          note={note}
+          onDelete={onDelete}
+          onCompleted={onCompleted}
+        />
       ))}
     </div>
   );
@@ -10,26 +15,39 @@ function NoteList({ notes }) {
 
 export default NoteList;
 
-function NoteItem({ note }) {
+function NoteItem({ note, onDelete, onCompleted }) {
   const options = {
     year: "numeric",
     month: "long",
     day: "numeric",
   };
+
   return (
-    <div className="note-item">
+    <div className={`note-item ${note.completed?"completed":""}`}>
       <div className="note-item__header">
         <div>
           <p className="title">{note.title}</p>
           <p className="description">{note.description}</p>
         </div>
-        <div  className="actions">
-          <button>❌</button>
-          <input type="checkbox" name="" id="" />
+        <div className="actions">
+          <button onClick={() => onDelete(note.id)}>❌</button>
+          <input
+            type="checkbox"
+            name={note.id}
+            id={note.id}
+            value={note.id}
+            checked={note.completed}
+            onChange={onCompleted}
+          />
         </div>
       </div>
       <div className="note-item__footer">
-        {new Date(note.createdAt).toLocaleDateString("en-US", options)}
+        <span>
+          {new Date(note.createdAt).toLocaleDateString("en-US", options)}
+        </span>
+        <span>
+          {" // " + new Date(note.createdAt).toLocaleTimeString("ar-EG", {})}
+        </span>
       </div>
     </div>
   );
